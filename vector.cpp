00c88e21ebc753d7040f0313ef54a4387bdb9509
dtn89 <dangtrungnam2809@gmail.com>
2024-10-21 10:54:13 +0700

    Update vector file

--- vector.cpp
+++ vector.cpp
@@ -10,7 +10,8 @@ int main()
     vector<int> items = {12, 13};
     items.push_back(100);
     cout << items.size() << endl;
-    cout << items[2] << endl;
-
+    cout << items[items.size() - 1] << endl;
+    items.pop_back();
+    cout << items[items.size() - 1] << endl;
     return 0;
 }
