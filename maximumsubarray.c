4838211074eb54316cd072399261943b14e119ab
dtn89 <dangtrungnam2809@gmail.com>
2024-10-21 21:04:53 +0700

    Update maxiumsubarry algorithm

--- maximumsubarray.c
+++ maximumsubarray.c
@@ -22,10 +22,23 @@ int maxSubarraySum(int arr[], int size)
     }
     return maxSum;
 }
+
+int maxSubarraySumAlgo(int arr[], int size)
+{
+    int res = arr[0];
+    int maxCurrSum = 0;
+    for(int i = 0; i < size; i++)
+    {
+        maxCurrSum = (maxCurrSum + arr[i] > arr[i]) ? maxCurrSum + arr[i] : arr[i];
+        res = (res > maxCurrSum) ? res : maxCurrSum;
+    }
+    return res;
+}
 int main()
 {
     int arr[] = {2, 3, -8, 7, -1, 2, 3};
     int size = sizeof(arr)/sizeof(arr[0]);
-    printf("Maximium sum of subarray = %d", maxSubarraySum(arr, size));
+    // printf("Maximium sum of subarray = %d", maxSubarraySum(arr, size));
+    printf("Maximum summary of subarry = %d", maxSubarraySumAlgo(arr, size));
     return 0;
 }
