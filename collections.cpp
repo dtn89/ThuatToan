014ea55526540d0978d8a5c3f002a504285edc49
dtn89 <dangtrungnam2809@gmail.com>
2024-10-21 09:13:39 +0700

    Update Collections file

--- collections.cpp
+++ collections.cpp
@@ -3,18 +3,22 @@
 
 using namespace std;
 
+
+void print_array(int array[], int size)
+{
+    for(int i = 0; i < size; i++)
+    {
+        cout << array[i] << "\t";
+    }
+
+}
 int main()
 {
     int guesses[] = {12, 43, 23, 43, 23};
 
     int size = sizeof(guesses)/sizeof(guesses[0]);
 
-    cout << size << endl;
-
-    for(int i = 0; i < size; i++)
-    {
-        cout << guesses[i] << endl;
-    }
+    print_array(guesses, size);
 
     return 0;
 }
