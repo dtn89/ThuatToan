a176095b81cdeb6950fcb0da685e18cfc3868163
dtn89 <dangtrungnam2809@gmail.com>
2024-10-21 21:46:25 +0700

    Update XOR algorithm to finding missing number

--- findMissingNumber.cpp
+++ findMissingNumber.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int missingNumber(int n, vector<int> arr)
+int missingNumber(int n, vector<int> &arr)
 {
 
     // Create hash array of size n+1
@@ -34,10 +34,26 @@ int missingNumber(int n, vector<int> arr)
     return -1;
 }
 
+int findMissingNumberUsingXor(int n, vector<int>& arr)
+{
+    int res_01 = 0, res_02 = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        res_01 ^= i;
+    }
+
+    for(int i = 0; i < n - 1; i++)
+    {
+        res_02 ^= arr[i];
+    }
+    return res_01 ^ res_02;
+}
 /* Driver code */
 int main()
 {
     vector<int> arr = { 6, 2, 3, 5, 1};
     int n = 6;
-    cout << missingNumber(n, arr);
+    // cout << missingNumber(n, arr);
+    cout << findMissingNumberUsingXor(n, arr);
+    return 0;
 }
