a1115f980c4d756e787789b2c55ecc75441386e2
dtn89 <dangtrungnam2809@gmail.com>
2024-10-29 09:17:05 +0700

    Adding constant pointer, pointer to constant, and constant pointer to constant

---
+++ constPointe.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+
+using namespace std;
+
+int main(void)
+{
+    // Constant pointers
+    double x1 = 11.1, x2 = 22.2;
+    double* const p1 = &x1; // Khai bao con tro hang va khoi tao
+    // p1 = &x2; // error: assignment of read-only variable 'p1'
+    cout << *p1 << endl;
+
+    // pointer to constant
+    float z = 33.3f;
+    const float* p2 = &z;
+    // *p2 = 1; // error: assignment of read-only location '*p2'
+    cout << *p2 << endl;
+
+    // const pointer to const
+    int m = 44, n = 55;
+    const int* const p3 = &m;
+    cout << *p3 << endl;
+
+    return 0;
+}
